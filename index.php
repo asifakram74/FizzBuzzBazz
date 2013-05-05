9e64233100c28e8865a71b7511a6e9f878f4dd25
Ahsan Atiq <iamahsan82@gmail.com>
2013-05-06 03:23:58 +0500

    Completed zz.class.php & index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+require_once 'zz.class.php';
+?>
+<html>
+	<head>
+		<title>Testing ZZ</title>
+	</head>
+	<body>
+		<form name="myform" action="" method="get">
+			Starting Value <input type="text" name="starting" value="">  
+			Ending Value <input type="text" name="ending" value="">  
+			<input type="submit" name="submit" value="Submit">
+		</form>
+		<?php 
+		if(isset($_REQUEST['starting']) && $_REQUEST['starting']!=="" && isset($_REQUEST['ending']) && $_REQUEST['ending']!=="")
+		{
+			$zz = new zz($_REQUEST['starting'], $_REQUEST['ending']);
+			$zz->output();			
+		}			
+		?>
+	</body>
+</html>
--- zz.class.php
+++ zz.class.php
@@ -2,82 +2,115 @@
 
 class zz {
 
-	private $x;
-	private $y;
-	public $errors = array();
-	private $words = array('Fizz', 'Buzz');
+	private $x; // starting value
+	private $y; // ending value
+	public $errors = array(); // errors array 
+	private $words = array('3'=>'Fizz', '5'=>'Buzz'); // zz words
 
 	public function __construct($x='' , $y= '') {
 		
-		if($x)
-			$this->x = $x;
-		if($y)
-			$this->y = $y;
+		if($x) // set x "starting" value if provided in initialization
+			$this->setStart($x);
+		if($y) // set y "ending" value if provided in initialization
+			$this->setEnd($y);
 		
 	}
 
 	public function __set($name, $value) {
 		
-		if($name=='x' || $name=='y') 
-		{
-			if(!is_integer($value))
-				return $this->setError('UNVALID_'.$name);
-			else
-				$this->unsetError('UNVALID_'.$name);
-		}
 
-		$this->$name = $value;
-	}
+		if($name=='x') // check if x "starting value" is set from outside
+			$this->setStart($value);
 
-	public function setStart($v='') {
+		elseif($name=='y') // check if y "ending value" is set from outside
+			$this->setEnd($value);
+		
+		else
+			$this->$name = $value;
+	}	
 
-		if($v=="")
+	public function setStart($v='') { // setter function for setting the starting value
+
+		if($v==="") // check if value is not provided
 			return $this->setError('START_MISSING');				
 		else
 			$this->unsetError('START_MISSING');				
+
+		if((string)(int)$v == $v && (int)$v >= 0) //check if value is positive integer
+			$this->unsetError('UNVALID_START');
+		else
+			return $this->setError('UNVALID_START');
+
 
 		$this->x = $v;
 
 	}
 
-	public function setEnd($v='') {
+	public function setEnd($v='') { // setter function for setting the ending value
 		
-		if($v=="")
+		if($v==="") // check if value is not provided
 			return $this->setError('END_MISSING');				
 		else
 			$this->unsetError('END_MISSING');				
 
+		if((string)(int)$v == $v && (int)$v >= 0) //check if value is positive integer
+			$this->unsetError('UNVALID_END');
+		else
+			return $this->setError('UNVALID_END');
+		
 		$this->y = $v;
 
 	}
 
-	public function output() {
+	public function output() { // prints the range / output
 		
-		if($this->x=="")
-			$this->setError('START_MISSING');
-		else
-			$this->unsetError('START_MISSING');				
-
-		if($this->y=="")
-			$this->setError('END_MISSING');
-		else
-			$this->unsetError('END_MISSING');				
-
-		if($errors = $this->getErrors())
+		if($this->getErrors()===false) //check if errors already triggered in set methods
 		{
-			foreach($errors as $error)
-				echo $error.'/r/n <br>';
+
+			if($this->x==="") // check if starting value is missing/not available
+				$this->setError('START_MISSING');
+			else
+				$this->unsetError('START_MISSING');				
+
+			if($this->y==="") // check if ending value is missing / not available
+				$this->setError('END_MISSING');
+			else
+				$this->unsetError('END_MISSING');					
+
+		}
+
+		if($this->getErrors()===false) //check if errors already triggered
+		{
+			if($this->x >= $this->y)
+				$this->setError('RANGE_MISMATCH');
+			else
+				$this->unsetError('RANGE_MISMATCH');
+		}	
+
+		if($errors = $this->getErrors()) //get errors again
+		{
+			foreach($errors as $error) 
+				echo $error.' <br>'; //print errors
 		}
 		else
 		{
-			$cwords = count($this->words);	
-			$c = 1
+			
+			$c = 1;
 			for($x=$this->x; $x<=$this->y; $x++)
 			{
+				$strike = false;
 				if($c!=1)
 				echo ', ';
 
+				foreach($this->words as $key=>$word)
+					if(($x%$key)==0)
+					{
+						echo $word;
+						$strike = true;
+					}
 
+				if($strike===false)		
+				echo $x;
 			$c++;	
 			}
 
@@ -89,15 +122,23 @@ class zz {
 	public function setError($key){
 		
 		switch($key) {
-			case 'UNVALID_x':
+			case 'UNVALID_START':
 				$this->errors[$key] = 'Please enter the valid (integer) starting value';
-			case 'UNVALID_y':
+				break;
+			case 'UNVALID_END':
 				$this->errors[$key] = 'Please enter the valid (integer) ending value';
+				break;
 			case 'START_MISSING':
 				$this->errors[$key] = 'Start value missing';
+				break;
 			case 'END_MISSING':
 				$this->errors[$key] = 'End value missing';
-			
+				break;		
+			case 'RANGE_MISMATCH':
+				$this->errors[$key] = 'Starting value cannot be less than or equal to ending value';
+				break;		
+			default:
+				$this->errors['NO_MATCH'] = 'key:'.$key.' not found in errors';		
 
 		}
 		return false;
@@ -114,7 +155,8 @@ class zz {
 		
 		if(count($this->errors)>0)
 			return $this->errors;
-		return true;
+
+		return false;
 
 	}
 
